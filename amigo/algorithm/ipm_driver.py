fixed post-optimality derivatives; added ability to set data values from the meta data entries in a model

amigo/algorithm/ipm_driver.py
@@ -8,12 +8,14 @@ or feasibility restoration.
 """
 
 import warnings
+import numpy as np
 
 # Raw pybind11 classes
 from ..amigo import InteriorPointOptimizer, Vector
 
-# Import from the model
+# Import from amigo classes
 from ..model import ModelVector
+from ..utils import tocsr
 
 # Optimizer imports from algorithm classes
 from .barrier_strategy import make_barrier_strategy
@@ -79,10 +81,16 @@ class Optimizer:
         self.lower = self.problem.get_lower()
         self.upper = self.problem.get_upper()
 
+        # The MPI communicator
         self.comm = comm
 
         # Set up the vectors
         self._create_interior_point_backend()
+
+        # Objects created during optimization that are saved
+        self.evaluator = None
+        self.state = None
+        self.solver = None
 
         return
 
@@ -130,14 +138,14 @@ class Optimizer:
         continuation_control = options["continuation_control"]
 
         # Class for evaluating problem-specific quantities
-        evaluator = Evaluator(self.problem, self.optimizer)
+        self.evaluator = Evaluator(self.problem, self.optimizer)
 
         # The interior point state object contains information about the design point, the
         # gradient and the Hessian of the Lagrangian.
-        state = InteriorPointState(self.x, options, self.problem, self.optimizer)
+        self.state = InteriorPointState(self.x, options, self.problem, self.optimizer)
 
         # Create the solver depending on options
-        solver = make_solver(options, state)
+        self.solver = make_solver(options, self.state)
 
         # The inertia correction
         inertia_corrector = InertiaCorrector(options, self.problem, self.optimizer)
@@ -165,35 +173,35 @@ class Optimizer:
         # Initialize the dual and slack variable values. This utilizes the solver object
         # to find initial values of the dual variables.
         slack_init = SlackInitializer(options, self.model, self.problem, self.optimizer)
-        slack_init.initialize_slacks(evaluator, state)
+        slack_init.initialize_slacks(self.evaluator, self.state)
 
         # Initialize the multipliers
         multiplier_init = MultiplierInitializer(options, self.problem, self.optimizer)
-        multiplier_init.initialize_multipliers(evaluator, solver, state)
+        multiplier_init.initialize_multipliers(self.evaluator, self.solver, self.state)
 
         # Set the initial status
         status = CONTINUE
 
         # Initialize the barrier strategy prior to optimization
-        barrier_strategy.initialize(evaluator, state)
+        barrier_strategy.initialize(self.evaluator, self.state)
 
         max_iters = options["max_iterations"]
         for counter in range(max_iters):
             # Update the iteration counter
-            state.iter = counter
+            self.state.iter = counter
 
             # Evaluate the objective and barrier function
-            evaluator.evaluate_objective_and_barrier(state)
+            self.evaluator.evaluate_objective_and_barrier(self.state)
 
             # Evaluate the residuals for the convergence check
-            evaluator.evaluate_residual(state)
+            self.evaluator.evaluate_residual(self.state)
 
             # Check for convergence based on the initial point
-            status = check.test_convergence(evaluator, state)
+            status = check.test_convergence(self.evaluator, self.state)
 
             # Log the information about the iteration and the status of all the
             # internal objects within the optimizer
-            logger.log_iteration(status, state)
+            logger.log_iteration(status, self.state)
 
             # Break if the check indicates we shouldn't continue
             if status != CONTINUE:
@@ -201,35 +209,41 @@ class Optimizer:
 
             # Callback for the continuation control.
             if continuation_control is not None:
-                continuation_control(state)
+                continuation_control(self.state)
 
             # Perform an update of the barrier parameter prior to any factorization or step
-            barrier_info = barrier_strategy.update_barrier(evaluator, state)
+            barrier_info = barrier_strategy.update_barrier(self.evaluator, self.state)
 
             # Let the line search object determine if a reset is appropriate based on the barrier parameter
             # update. For instance, this call may reset the filter.
-            line_search.reset_on_new_barrier(state, barrier_info)
+            line_search.reset_on_new_barrier(self.state, barrier_info)
 
             # Factor the KKT system considering the inertia.
             # TODO: Implement a inertia info class
-            factor_ok = inertia_corrector.factor_for_inertia(solver, evaluator, state)
+            factor_ok = inertia_corrector.factor_for_inertia(
+                self.solver, self.evaluator, self.state
+            )
 
             do_feasible_resto = True
             if factor_ok:
-                # Compute the direction and store in state.step. This should be a descent direction
+                # Compute the direction and store in self.state.step. This should be a descent direction
                 # because of the inertia check
-                newton_step.compute_step(solver, evaluator, state)
+                newton_step.compute_step(self.solver, self.evaluator, self.state)
 
                 # Using the same factorization and solver, assess whether a correction step is required
                 # and compute it.
-                barrier_strategy.add_step_correction(solver, evaluator, state)
+                barrier_strategy.add_step_correction(
+                    self.solver, self.evaluator, self.state
+                )
 
                 # Perform a line search along the step direction
-                line_search_info = line_search.line_search(solver, evaluator, state)
+                line_search_info = line_search.line_search(
+                    self.solver, self.evaluator, self.state
+                )
 
                 # Assess what happened after the line search
                 barrier_strategy.update_after_line_search(
-                    line_search_info, evaluator, state
+                    line_search_info, self.evaluator, self.state
                 )
 
                 if line_search_info.success:
@@ -238,15 +252,94 @@ class Optimizer:
             # If the line search was not successful, perform feasibility restoration
             if do_feasible_resto:
                 warnings.warn("Feasibility restoration phase not implemented")
-                # feasible_resto.restoration_phase(solver, evaluator, state)
+                # feasible_resto.restoration_phase(solver, self.evaluator, self.state)
 
         else:
             # The optimization for loop completed normally, so we did not converge
             # Check the convergence status
-            state.iter = max_iters
-            status = check.test_convergence(evaluator, state)
+            self.state.iter = max_iters
+            status = check.test_convergence(self.evaluator, self.state)
 
             # Log the iteration
-            logger.log_iteration(status, state)
+            logger.log_iteration(status, self.state)
 
         return logger.get_data()
+
+    def compute_output(self, output: ModelVector | Vector | None = None):
+        """Evaluate model outputs at the final iterate."""
+
+        if output is None:
+            if self.model is None:
+                output = self.model.create_output_vector()
+            else:
+                output = self.problem.create_output_vector()
+
+        if isinstance(output, ModelVector):
+            out_vec = output.get_vector()
+        elif isinstance(output, Vector):
+            out_vec = output
+        else:
+            raise RuntimeError(
+                "output vector is not an instance of Vector or ModelVector"
+            )
+
+        self.problem.compute_output(self.x, out_vec)
+        return output
+
+    def compute_post_opt_derivatives(self, of=[], wrt=[], method="adjoint"):
+        """
+        Compute the post-optimality derivatives of the outputs.
+
+        Parameters
+        ----------
+        of, wrt : list of str
+            Output and input variable names.
+        method : {"adjoint", "direct"}
+            Use adjoint when len(of) < len(wrt), direct otherwise.
+        """
+
+        if self.state is None:
+            raise RuntimeError("Call optimize() before compute_post_opt_derivatives")
+
+        of_indices, of_map = self.model.get_indices_and_map(of)
+        wrt_indices, wrt_map = self.model.get_indices_and_map(wrt)
+
+        dfdx = np.zeros((len(of_indices), len(wrt_indices)))
+
+        out_wrt_input = self.problem.create_output_jacobian_wrt_input()
+        self.problem.output_jacobian_wrt_input(self.x, out_wrt_input)
+        out_wrt_input = tocsr(out_wrt_input)
+
+        out_wrt_data = self.problem.create_output_jacobian_wrt_data()
+        self.problem.output_jacobian_wrt_data(self.x, out_wrt_data)
+        out_wrt_data = tocsr(out_wrt_data)
+
+        grad_wrt_data = self.problem.create_gradient_jacobian_wrt_data()
+        self.problem.gradient_jacobian_wrt_data(self.x, grad_wrt_data)
+        grad_wrt_data = tocsr(grad_wrt_data)
+
+        self.evaluator.evaluate_hessian(self.state)
+        self.evaluator.evaluate_diagonal(self.state)
+
+        self.solver.factor(self.state.hessian, self.state.diagonal)
+
+        psi = self.problem.create_vector()
+        res = self.problem.create_vector()
+
+        if method == "adjoint":
+            for i in range(len(of_indices)):
+                idx = of_indices[i]
+                res.get_array()[:] = -out_wrt_input[idx, :].toarray()
+                self.solver.solve(res, psi)
+                adjx = grad_wrt_data.T @ psi.get_array()
+                dfdx[i, :] = out_wrt_data[idx, wrt_indices] + adjx[wrt_indices]
+        elif method == "direct":
+            grad_wrt_data = grad_wrt_data.tocsc()
+            for i in range(len(wrt_indices)):
+                idx = wrt_indices[i]
+                res.get_array()[:] = -grad_wrt_data[:, idx].toarray().flatten()
+                self.solver.solve(res, psi)
+                dirx = out_wrt_input @ psi.get_array()
+                dfdx[:, i] = out_wrt_data[of_indices, idx] + dirx[of_indices]
+
+        return dfdx, of_map, wrt_map

amigo/model.py
@@ -440,16 +440,20 @@ class ModelVector:
             self[d["comp"]] = d["data"]
         return
 
+    def copy(self, src: Self):
+        self._x.copy(src._x)
+
 
 class MetaView:
     """This class enables meta to be set like a vector"""
 
-    def __init__(self, model, meta_name):
+    def __init__(self, model, meta_name, vtype="vars"):
         self.model = model
         self.meta_name = meta_name
+        self.vtype = vtype
 
     def __setitem__(self, key, value):
-        self.model.set_meta(self.meta_name, key, value)
+        self.model.set_meta(self.meta_name, key, value, vtype=self.vtype)
 
 
 class Model:
@@ -475,7 +479,6 @@ class Model:
         self.links = []
 
         # Staged values that are applied to the model at initialization
-        self._staged_data = []
         self._staged_fixed_vars = []
         self._staged_meta_data = []
 
@@ -640,17 +643,13 @@ class Model:
         ) in model.links:
             self.link(name + "." + src_expr, name + "." + tgt_expr, src_idx, tgt_idx)
 
-        # Add staged data
-        for data_name, data in model._staged_data:
-            self._staged_data.append((name + "." + data_name, data))
-
         # Add fixed variables
         for expr, indices in model._staged_fixed_vars:
             self._staged_fixed_vars.append((name + "." + expr, indices))
 
         # Add staged meta data
-        for meta, expr, data in model._staged_meta_data:
-            self._staged_fixed_vars.append((meta, name + "." + expr, data))
+        for vtype, meta, expr, data in model._staged_meta_data:
+            self._staged_meta_data.append((vtype, meta, name + "." + expr, data))
 
         return
 
@@ -815,7 +814,7 @@ class Model:
             d = self.problem.get_data_vector()
             d.get_array()[self.get_indices(name)] = data
         else:
-            self._staged_data.append((name, data))
+            self.set_meta("value", name, data, vtype="data")
         return
 
     def add_fixed(self, expr: str, indices: None | list | np.ndarray = None):
@@ -830,13 +829,17 @@ class Model:
         self._staged_fixed_vars.append((expr, indices))
         return
 
-    def get_meta_view(self, meta_name: str):
+    def get_meta_view(self, meta_name: str, vtype: str = "vars"):
         """
         Get a MetaView object to set the meta data like vector components
         """
-        return MetaView(self, meta_name)
+        if not vtype in ["vars", "output", "data"]:
+            raise ValueError("vtype must be in vars, output or data")
+        return MetaView(self, meta_name, vtype=vtype)
 
-    def set_meta(self, meta_name: str, name: str, data: List | np.ndarray):
+    def set_meta(
+        self, meta_name: str, name: str, data: List | np.ndarray, vtype: str = "vars"
+    ):
         """
         Set meta data into the model.
 
@@ -846,12 +849,15 @@ class Model:
             meta_name (str): Name of the meta data argument
             expr (str): Name of the data
             data (float, list, np.ndarray): Data values
+            vtype (str): Variable type, one of "vars", "output", "data"
         """
 
         if self._initialized:
             raise RuntimeError("Cannot set meta data values after initialization")
+        if not vtype in ["vars", "output", "data"]:
+            raise ValueError("vtype must be in vars, output or data")
 
-        self._staged_meta_data.append((meta_name, name, data))
+        self._staged_meta_data.append((vtype, meta_name, name, data))
 
         return
 
@@ -919,9 +925,7 @@ class Model:
         self._get_values_from_meta("value", self.get_initial_point())
         self._get_values_from_meta("lower", self.get_lower())
         self._get_values_from_meta("upper", self.get_upper())
-
-        # Now apply any staged data
-        self._apply_staged_data()
+        self._get_values_from_meta("value", self.get_data_vector(), vtype="data")
 
         return
 
@@ -1365,19 +1369,14 @@ amigo_add_python_module(
         model_data["links"] = link_data
 
         # Serialize the staged data
-        staged_data = []
-        for name, data in self._staged_data:
-            staged_data.append((name, _to_list(data)))
-        model_data["staged_data"] = staged_data
-
         fixed_vars = []
         for name, indices in self._staged_fixed_vars:
             fixed_vars.append((name, _to_list(indices)))
         model_data["fixed_vars"] = fixed_vars
 
         staged_meta = []
-        for meta_name, name, data in self._staged_meta_data:
-            staged_meta.append((meta_name, name, _to_list(data)))
+        for vtype, meta_name, name, data in self._staged_meta_data:
+            staged_meta.append((vtype, meta_name, name, _to_list(data)))
         model_data["staged_meta"] = staged_meta
 
         return model_data
@@ -1404,17 +1403,13 @@ amigo_add_python_module(
             obj.link(src[0], tgt[0], src_indices=src[1], tgt_indices=tgt[1])
 
         # Deserialize the staged information
-        staged_data = model_data["staged_data"]
-        for name, data in staged_data:
-            obj.set_data(name, data)
-
         fixed_vars = model_data["fixed_vars"]
         for name, indices in fixed_vars:
             obj.add_fixed(name, indices)
 
         staged_meta = model_data["staged_meta"]
-        for meta_name, name, data in staged_meta:
-            obj.set_meta(meta_name, name, data)
+        for vtype, meta_name, name, data in staged_meta:
+            obj.set_meta(meta_name, name, data, vtype=vtype)
 
         return obj
 
@@ -1668,29 +1663,17 @@ amigo_add_python_module(
 
         return np.nonzero(temp)[0]
 
-    def _apply_staged_data(self):
-        """Set the data that was staged"""
-        d_array = self.problem.get_data_vector().get_array()
-
-        for name, data in self._staged_data:
-            indices = self.get_indices(name)
-            d_array[indices] = data
-
-        return
-
     def _create_opt_problem(self, comm=COMM_WORLD):
         """
         Create the optimization problem object that is used to evaluate the gradient and
         Hessian of the Lagrangian.
 
-        After slack allocation, ``self.num_variables`` includes both the
-        original variables (design vars + constraint multipliers) and the
-        newly appended slack variables.  Slacks are marked as primal
-        (``is_multiplier = 0``) so that the C++ InteriorPointOptimizer
-        treats them as bounded design variables.  The original inequality
-        constraint rows remain marked as multipliers but their bounds will
-        be converted to equalities (``lb = ub = 0``) in the Optimizer,
-        reflecting the reformulation ``c(x) - s = 0``.
+        After slack allocation, ``self.num_variables`` includes both the original variables
+        (design vars + constraint multipliers) and the newly appended slack variables.
+        Slacks are marked as primal so that the C++ InteriorPointOptimizer treats them as
+        bounded design variables. The original inequality constraint rows remain marked as
+        multipliers but their bounds will be converted to equalities (``lb = ub = 0``) in
+        the Optimizer, reflecting the reformulation ``c(x) - s = 0``.
         """
 
         if not self._initialized:
@@ -1801,11 +1784,12 @@ amigo_add_python_module(
                     upper[comp.vars[var_name]] = meta["upper"]
 
         # Get any staged meta data
-        for meta_name, name, data in self._staged_meta_data:
-            if "lower" == meta_name:
-                lower[self.get_indices(name)] = data
-            if "upper" == meta_name:
-                upper[self.get_indices(name)] = data
+        for vtype, meta_name, name, data in self._staged_meta_data:
+            if vtype == "vars":
+                if "lower" == meta_name:
+                    lower[self.get_indices(name)] = data
+                if "upper" == meta_name:
+                    upper[self.get_indices(name)] = data
 
         # Find those constraints corresponding to inequalities
         mask = lower[con_indices] < upper[con_indices]
@@ -1892,7 +1876,10 @@ amigo_add_python_module(
             )
 
     def _get_values_from_meta(
-        self, meta_name: str, x: ModelVector | np.ndarray | None = None
+        self,
+        meta_name: str,
+        x: ModelVector | np.ndarray | None = None,
+        vtype: str = "vars",
     ):
         """
         Return a vector with meta data values for all variables including slacks.
@@ -1914,31 +1901,49 @@ amigo_add_python_module(
             raise RuntimeError("Must call initialize before get_values_from_meta")
 
         if x is None:
-            x = self.create_vector()
+            if vtype == "vars":
+                x = self.create_vector()
+            elif vtype == "data":
+                x = self.create_data_vector()
+            elif vtype == "output":
+                x = self.create_output_vector()
 
         # Set the entire vector to the default meta value
         x[:] = Meta.get_default_value(meta_name)
 
         # Modify the default values for all constraints so that they
         # default to equalitie constraints
-        if meta_name in ("lower", "upper"):
+        if vtype == "vars" and meta_name in ("lower", "upper"):
             x[self.constraint_indices] = 0.0
 
         # Get the meta data from values set in the components
         for comp_name, comp in self.comp.items():
-            for var_name in comp.vars:
-                name = comp_name + "." + var_name
-                meta = self.get_meta(name)
-                if meta.is_value_set(meta_name):
-                    x[comp.vars[var_name]] = meta[meta_name]
+            if vtype == "vars":
+                for var_name in comp.vars:
+                    name = comp_name + "." + var_name
+                    meta = self.get_meta(name)
+                    if meta.is_value_set(meta_name):
+                        x[comp.vars[var_name]] = meta[meta_name]
+            elif vtype == "data":
+                for var_name in comp.data:
+                    name = comp_name + "." + var_name
+                    meta = self.get_meta(name)
+                    if meta.is_value_set(meta_name):
+                        x[comp.data[var_name]] = meta[meta_name]
+            elif vtype == "output":
+                for var_name in comp.outputs:
+                    name = comp_name + "." + var_name
+                    meta = self.get_meta(name)
+                    if meta.is_value_set(meta_name):
+                        x[comp.outputs[var_name]] = meta[meta_name]
 
         # Get any staged meta data
-        for meta_name_, name, data in self._staged_meta_data:
-            if meta_name == meta_name_:
+        for vtype_, meta_name_, name, data in self._staged_meta_data:
+            if vtype == vtype_ and meta_name == meta_name_:
                 x[self.get_indices(name)] = data
 
         # Deal with the slack variables
-        if meta_name in ("lower", "upper"):
+        if vtype == "vars" and meta_name in ("lower", "upper"):
             # Set bounds for the inequality constraints
             x[self.ineq_constraint_indices] = 0.0
 